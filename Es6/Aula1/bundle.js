104c58cc94b66e8ead56d06cfadfe95d69b0e3c0
Scrowszinho <gustascrows@gmail.com>
2020-12-07 11:04:26 -0300

    Comentarios Adicionados

--- Es6/Aula1/bundle.js
+++ Es6/Aula1/bundle.js
@@ -1,5 +1,21 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -21,26 +37,65 @@ var Matematica = /*#__PURE__*/function () {
   return Matematica;
 }();
 
-console.log(Matematica.soma(1, 2)); // class list {
-//     constructor() {
-//         this.data = [];
-//     }
-//     add(data) {
-//         this.data.push(data);
-//         console.log(this.data);
-//     }
-// }
-// class TodoList extends list {
-//     constructor() {
-//         super();
-//         this.usuario = 'Gustavo';
-//     }
-//     mostraUser() {
-//         console.log(this.usuario);
-//     }
-// }
-// const minhaLista = new TodoList();
-// document.getElementById('adcionar').onclick = function () {
-//     minhaLista.add('Novo Todo');
-// }
-// minhaLista.mostraUser();
+console.log(Matematica.soma(1, 2)); // Conceito de classe em JS
+// Juntament com o static
+// 
+
+var list = /*#__PURE__*/function () {
+  function list() {
+    _classCallCheck(this, list);
+
+    this.data = [];
+  }
+
+  _createClass(list, [{
+    key: "add",
+    value: function add(data) {
+      this.data.push(data);
+      console.log(this.data);
+    }
+  }]);
+
+  return list;
+}(); // Criação da classe pai, onde cria-se um array
+// e adciona um elemento através da função add com 
+// variavel data como parametro. 
+//
+
+
+var TodoList = /*#__PURE__*/function (_list) {
+  _inherits(TodoList, _list);
+
+  var _super = _createSuper(TodoList);
+
+  function TodoList() {
+    var _this;
+
+    _classCallCheck(this, TodoList);
+
+    _this = _super.call(this);
+    _this.usuario = 'Gustavo';
+    return _this;
+  }
+
+  _createClass(TodoList, [{
+    key: "mostraUser",
+    value: function mostraUser() {
+      console.log(this.usuario);
+    }
+  }]);
+
+  return TodoList;
+}(list); // Criação da classe filha TodoList
+// extendida da classe lista
+// Que cria uma variavel e função de exibila
+// e herda as as outras variaveis
+
+
+var minhaLista = new TodoList(); // Criando uma variavél para executar a classe TodoList
+
+document.getElementById('adcionar').onclick = function () {
+  minhaLista.add('Novo Todo');
+};
+
+minhaLista.mostraUser(); // Passando valores e exibindo o usuario
--- Es6/Aula1/main.js
+++ Es6/Aula1/main.js
@@ -3,35 +3,46 @@ class Matematica{
         return a + b;
     }
 }
-
 console.log(Matematica.soma(1,2));
-
-// class list {
-//     constructor() {
-//         this.data = [];
-//     }
-//     add(data) {
-//         this.data.push(data);
-//         console.log(this.data);
-//     }
-// }
+// Conceito de classe em JS
+// Juntament com o static
 
 
-// class TodoList extends list {
-//     constructor() {
-//         super();
-//         this.usuario = 'Gustavo';
-//     }
+// 
 
-//     mostraUser() {
-//         console.log(this.usuario);
-//     }
+class list {
+    constructor() {
+        this.data = [];
+    }
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+// Criação da classe pai, onde cria-se um array
+// e adciona um elemento através da função add com 
+// variavel data como parametro. 
+//
+class TodoList extends list {
+    constructor() {
+        super();
+        this.usuario = 'Gustavo';
+    }
 
-// }
+    mostraUser() {
+        console.log(this.usuario);
+    }
+}
+// Criação da classe filha TodoList
+// extendida da classe lista
+// Que cria uma variavel e função de exibila
+// e herda as as outras variaveis
 
-// const minhaLista = new TodoList();
+const minhaLista = new TodoList();
+// Criando uma variavél para executar a classe TodoList
 
-// document.getElementById('adcionar').onclick = function () {
-//     minhaLista.add('Novo Todo');
-// }
-// minhaLista.mostraUser(); 
+document.getElementById('adcionar').onclick = function () {
+    minhaLista.add('Novo Todo');
+}
+minhaLista.mostraUser(); 
+// Passando valores e exibindo o usuario
