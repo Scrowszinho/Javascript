e3c03120a07cf9f164351b18db48f956f0aab29c
Scrowszinho <gustascrows@gmail.com>
2020-12-16 13:44:51 -0300

    feat: Inlcuindo as noticias do banco de daso SQL

--- Node/Aula 01/app.js
+++ Node/Aula 01/app.js
@@ -2,7 +2,7 @@ var app = require('./config/server');
 var rotaHome = require('./app/routes/home')(app);
 var rotaNoticias = require('./app/routes/noticias')(app);
 var rotaForm = require('./app/routes/formularioInclusao')(app);
-
+ 
 
 app.listen(8080, () =>{
 console.log('Servidor on 🚀');
--- Node/Aula 01/app/routes/noticias.js
+++ Node/Aula 01/app/routes/noticias.js
@@ -1,5 +1,23 @@
-module.exports = (app) =>{
-app.get('/noticias', (request, response)=>{
-    response.render('noticias/noticias');
-} );
+const { response } = require('../../config/server');
+
+module.exports = (app) => {
+
+    app.get('/noticias', (request, response) => {
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '1234',
+            database: 'portal_noticias'
+        });
+        // Importando os dados do Mysql
+        // e guardando-os na variavel connection
+
+        connection.query('select * from noticias', (error, result) => {
+            response.render("noticias/noticias",{noticias: result});
+        });
+        // Passando uma resposta em Json para o site
+
+        
+    });
 }
