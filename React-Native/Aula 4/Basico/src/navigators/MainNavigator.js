abdd7eb2648e9ea1f6ac2a60fd31d4035a2952bb
Luis Gustavo <gustascrows@gmail.com>
2021-02-02 20:09:25 -0300

    Feat: Aula 4 Basico sobre React Navigation v4

---
+++ React-Native/Aula 4/Basico/src/navigators/MainNavigator.js
@@ -0,0 +1,14 @@
+import {createStackNavigator} from 'react-navigation-stack';
+import Home from './screens/Home';
+import Login from './screens/Login';
+
+const MainNavigator = createStackNavigator({
+  Home:{
+      screen:Home
+  },
+  Login:{
+      screen:Login
+  }
+});
+
+export default MainNavigator;
---
+++ React-Native/Aula 4/Basico/src/navigators/screens/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, Text } from '../../Style';
+
+export default function Login(props) {
+    return (
+      <SafeAreaView>
+        <Text>Login</Text>
+        <Text>Nome: {props.navigation.state.params.nome}</Text>
+      </SafeAreaView>
+    );
+  
+  }
+  
