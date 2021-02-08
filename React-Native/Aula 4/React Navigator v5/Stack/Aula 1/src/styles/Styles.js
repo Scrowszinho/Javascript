2500c77d172229af8b18bac4fc6cf35c094cfe48
Luis Gustavo <gustascrows@gmail.com>
2021-02-08 16:21:35 -0300

    Feat: Aula 1 React Navigator 5

---
+++ React-Native/Aula 4/React Navigator v5/Stack/Aula 1/src/styles/Styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const View = styled.SafeAreaView`
+    flex: 1;
+    background-color: #fff;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Text = styled.Text`
+`;
---
+++ React-Native/Aula 4/React Navigator v5/Stack/Aula 1/src/views/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from '../styles/Styles';
+
+function HomeScreen(){
+    return(
+ <View>
+ <Text>Tela Home</Text>
+</View>
+    );
+}
+
+export default HomeScreen;  
