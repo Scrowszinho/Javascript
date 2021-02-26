04a8c4655a8cd3396a3201e8d39dd8a8ec1e13ad
Luis Gustavo <gustascrows@gmail.com>
2021-02-26 18:35:06 -0300

    Feat: Badge no icone

---
+++ React-Native/Aula 4/React Navigator v5/Tab/Aula 4/src/views/AboutScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from '../styles/Styles';
+
+function AboutScreen() {
+    return (
+        <View>
+            <Text>Tela de Sobre</Text>
+        </View>
+    );
+}
+
+export default AboutScreen;  
